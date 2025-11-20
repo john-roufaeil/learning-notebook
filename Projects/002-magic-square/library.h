613e515b702fa083e4178b5d2cfbdab44e9771b2
john <john.flouis1@gmail.com>
2025-11-20 02:39:12 +0200

    re implement 002 magic square

--- Projects/002-magic-square/library.h
+++ Projects/002-magic-square/library.h
@@ -1,21 +1,20 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+// #include <vector>
+// #include <functional>
 #include <string>
-#include <vector>
-#include <functional>
+#include <iostream>
 
-// Supported colors: "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white", "reset"
-void textColor(const std::string& color);
-void gotoxy(int x, int y);
-void clearScreen();
-void getTerminalSize(int& width, int& height);
-void displayInstructions(const std::string instructionsMessage, int x, int y);
-void delay(int milliseconds);
-void typeText(std::string message, int delayTime = 50);
+#ifdef _WIN32
+    #include <windows.h>
+    #include <conio.h>
+#else
+    #include <unistd.h>
+    #include <termios.h>
+#endif
 
-// Keyboard input functions
-enum class Key {
+enum Key {
     UP,
     DOWN,
     LEFT,
@@ -27,28 +26,14 @@ enum class Key {
     END,
     UNKNOWN
 };
-Key getKeyPress();
 
-// Menu system
-struct MenuItem {
-    std::string text;
-    std::function<void()> action;
-};
-
-class Menu {
-private:
-    std::vector<MenuItem> items;
-    int selectedIndex;
-    int x, y; // Menu position
-    std::string title;
-
-public:
-    Menu(const std::string& menuTitle, int posX = 5, int posY = 3);
-
-    void addItem(const std::string& text, std::function<void()> action);
-    void display();
-    void run();
-    int getSelectedIndex() const { return selectedIndex; }
-};
+void gotoxy(int x, int y);
+int getColorCode(std::string color, int isWindows);
+void changeColor(std::string textColor);
+void resetColor();
+void clearScreen();
+Key getKeyPress();;
+void delay(int milliseconds);
+void typeText(std::string message, int delayTime = 50);
 
 #endif
--- Projects/002-magic-square/main.cpp
+++ Projects/002-magic-square/main.cpp
@@ -2,37 +2,28 @@
 #include <iostream>
 
 void buildMagicSquareBoxes(int n, int x, int y) {
-    // Draw horizontal lines and top border
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j < n; j++) {
             gotoxy(x + j * 4, y + i * 2);
             typeText("+---", 10);
         }
-        gotoxy(x + n * 4, y + i * 2);
         typeText("+", 10);
     }
-    
-    // Draw vertical lines
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j <= n; j++) {
             gotoxy(x + j * 4, y + i * 2 + 1);
-            typeText("|", 10);
-            if (j < n) {
-                typeText("   ", 10);
-            }
+            typeText("|   ", 10);
         }
     }
-    gotoxy(x, y);
 }
 
 void populateMagicSquare(int n, int x, int y) {
-    // Start at the middle of the first row
     int row = 0;
     int col = n / 2;
     
     for (int i = 1; i <= n * n; i++) {
         delay(250);
-        textColor("random");
 
         gotoxy(x + col * 4 + (i >= 100 ? 1 : 2), y + row * 2 + 1);
         typeText(std::to_string(i));
@@ -48,12 +39,10 @@ void populateMagicSquare(int n, int x, int y) {
 
 void magicScreen() {
     clearScreen();
-    textColor("cyan");
+    changeColor("cyan");
     gotoxy(4, 2);
     std::cout << "=== MAGIC SQUARE ===";
-    textColor("reset");
-    displayInstructions("Builds a magic square based on odd user input.", 4, 2);
-    displayInstructions("Use backspace to go to main menu.", 4, 3);
+    changeColor("reset");
 
     int n=0;
     gotoxy(4, 4);
@@ -62,9 +51,9 @@ void magicScreen() {
     
     while (n % 2 == 0 || n < 1 || n > 31) {
         gotoxy(4, 5);
-        textColor("red");
+        changeColor("red");
         typeText("Invalid input. Please enter a positive odd number between 1 and 31.");
-        textColor("reset");
+        changeColor("reset");
         gotoxy(4, 4);
         std::cout << "Enter an odd number for the size of the magic square:               ";
         gotoxy(58, 4);
@@ -74,22 +63,22 @@ void magicScreen() {
     }
 
     gotoxy(4, 6);
-    delay(500);
+    delay(250);
     typeText("Generating Magic Square for input " + std::to_string(n));
     gotoxy(4, 7);
-    delay(500);
+    delay(250);
     typeText("Rows and columns will sum up to " + std::to_string(n * (n * n + 1) / 2));
     gotoxy(4, 8);
-    delay(500);
+    delay(250);
     std::cout << "______________________________________" << std::endl;
 
     buildMagicSquareBoxes(n, 4, 10);
     populateMagicSquare(n, 4, 10);
     gotoxy(4, 10 + n * 2 + 2);
-    textColor("green");
+    changeColor("green");
     typeText("Magic Square of size " + std::to_string(n) + " completed!");
     gotoxy(4, 10 + n * 2 + 3);
-    textColor("reset");
+    changeColor("reset");
 
     while (true) {
         Key key = getKeyPress();
@@ -100,12 +89,64 @@ void magicScreen() {
 }
 
 int main() {
-    Menu mainMenu("Main Menu", 4, 2);
-    mainMenu.addItem("Magic Square", magicScreen);
-    mainMenu.addItem("Exit", []() {
+    std::string menuItems[] = { "Magic Square", "Exit" };
+    int menuSize = sizeof(menuItems) / sizeof(menuItems[0]);
+    int selectedIndex = 0;
+
+    while (true) {
         clearScreen();
-        exit(0);
-    });
-    mainMenu.run();
+        changeColor("cyan");
+        gotoxy(4, 2);
+        std::cout << "=== MAIN MENU ===";
+        resetColor();
+
+        for (short i = 0; i < menuSize; i++) {
+            gotoxy(4, 4 + i);
+            if (i == selectedIndex) {
+                changeColor("lightmagenta");
+                std::cout << "> " << menuItems[i];
+                resetColor();
+            } else {
+                std::cout << "  " << menuItems[i];
+            }
+        }
+
+        Key key = getKeyPress();
+        switch (key) {
+            case UP:
+            case LEFT:
+                if (selectedIndex == 0) selectedIndex = menuSize - 1;
+                else selectedIndex--;
+                break;
+            case DOWN:
+            case RIGHT:
+                if (selectedIndex == menuSize - 1)
+                    selectedIndex = 0;
+                else selectedIndex++;
+                break;
+            case HOME:
+                selectedIndex = 0;
+                break;
+            case END:
+                selectedIndex = menuSize - 1;
+                break;
+            case ENTER:
+                if (menuItems[selectedIndex] == "Magic Square") {
+                    magicScreen();
+                } else if (menuItems[selectedIndex] == "Exit") {
+                    clearScreen();
+                    exit(0);
+                }
+                break;
+            case ESCAPE:
+                clearScreen();
+                exit(0);
+            default:
+                break;
+        }
+    }
+
+
+
     return 0;
 }
