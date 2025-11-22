62310fa4f3c074e32c78a06ded2b4a9cec15b492
john <john.flouis1@gmail.com>
2025-11-22 02:30:29 +0200

    P006 2d array

---
+++ Projects/006-2d-array/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+void display2DArray(int arr[][5], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << *(*(arr + i) + j) << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+}
+
+int main(void) {
+    int tdArr[3][5] = {
+        {01, 02, 03, 04, 05},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15}};
+    display2DArray(tdArr, 3, 5);
+
+    return 0;
+}
+
+//tdArr is ptr to tdArr[0]
+//tdArr + i is ptr to tdArr[i]
+//*(tdArr + i) is tdArr[i] which is ptr to tdArr[i][0]
+//*( *(tdArr + i) + j) is tdArr[i][j]
