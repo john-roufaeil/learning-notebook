b61e687ad3ec3124936d1f3b565ec981e40a329a
john <john.flouis1@gmail.com>
2025-11-29 18:42:58 +0200

    P009 feat: fullprint, [], << overloads

--- Projects/009-string-class/string.cpp
+++ Projects/009-string-class/string.cpp
@@ -1,12 +1,12 @@
 #include "string.h"
 #include <stdexcept>
+#include <iostream>
 
 void String::setSize(int newSize) {
-    if (newSize < 0) {
-        throw std::invalid_argument("Size cannot be negative");
-    }
+    if (newSize < 1) throw std::invalid_argument("Size cannot be less than 1");
+
     char *newStr = new char[newSize];
-    int limit = (newSize < size) ? newSize - 1: this->size - 1;
+    int limit = (newSize < this->size) ? newSize - 1: this->size - 1;
     for (int i = 0; i < limit; i++) {
         newStr[i] = this->str[i];
     }
@@ -60,4 +60,18 @@ String::String(const char* inStr) {
 
 String::~String() {
     delete[] this->str;
+}
+
+void String::fullPrint() {
+    std::cout << "Size: " << this->getSize() << " chars, Data: " << this->getStr() << std::endl;
+}
+
+char& String::operator[](int idx) {
+    if (idx >= this-> size) throw std::invalid_argument("Index out of bound.");
+    return this->str[idx];
+}
+
+std::ostream& operator<<(std::ostream& os, String& s) {
+    os << s.getStr();
+    return os;
 }
--- Projects/009-string-class/string.h
+++ Projects/009-string-class/string.h
@@ -1,20 +1,28 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include<iostream>
+
 class String {
     private:
         int size;
         char* str;
 
     public:
-        String(int s = 10);
         String(const char* str = "");
+        String(int n);
         ~String();
         int getSize();
         void setSize(int n);
         char *getStr();
         void setStr(char *);
-        //copy
+        void fullPrint();
+        char& operator[](int idx);
+        // copy
 };
+
+std::ostream &operator<<(std::ostream &os, String &s);
+// std::istream &operator>>(std::istream &is, String &s);
+
 
 #endif
