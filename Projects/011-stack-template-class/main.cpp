76714e9a0a768ff5913a4ba90e0f491dfc3b1fd1
john <john.flouis1@gmail.com>
2025-12-02 01:35:21 +0200

    P011 feat: Stack Template Class

---
+++ Projects/011-stack-template-class/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include "stack.h"
+
+int main() {
+    Stack<int> s(5);
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    std::cout << "Stack after pushes: ";
+    s.print();
+
+    std::cout << "Popped: " << s.pop() << "\n";
+    std::cout << "Popped: " << s.pop() << "\n";
+    std::cout << "Popped: " << s.pop() << "\n";
+
+    std::cout << "Stack after pop: ";
+    s.print();
+
+    std::cout << "Is empty? " << s.isEmpty() << "\n";
+    std::cout << "Is full? " << s.isFull() << "\n";
+
+    s.push(40);
+    s.push(50);
+    s.push(60);
+    s.push(40);
+    s.push(50);
+    // s.push(40);
+    // s.push(50);
+    // s.push(60);
+    // s.push(40);
+    // s.push(50);
+
+    std::cout << "Stack after filling: ";
+    s.print();
+    std::cout << "Is empty? " << s.isEmpty() << "\n";
+    std::cout << "Is full? " << s.isFull() << "\n";
+
+    Stack s2 = Stack<int>();
+    std::cout << "New stack created: ";
+    s2.print();
+    s2 = s;
+    std::cout << "New stack copied: ";
+    s2.print();
+
+    Stack<std::string> s3;
+    std::cout << "String stack: ";
+    s3.print();
+    s3.push("Hello");
+    s3.push("World");
+    s3.push("I'm");
+    s3.push("a");
+    s3.push("Template");
+    s3.push("Stack");
+    s3.print();
+
+    std::cout << "Copy constructor string: ";
+    Stack<std::string> s4 = s3;
+    s4.print();
+
+    return 0;
+}
---
+++ Projects/011-stack-template-class/stack.h
@@ -0,0 +1,91 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <iostream>
+#include <stdexcept>
+
+template <typename T>
+class Stack {
+    private:
+        T *ptr;
+        int size;
+        int topIdx;
+    public:
+        Stack(int size = 10) {
+            if (size <= 0) {
+                throw std::invalid_argument("Stack size cannot be non positive.");
+            }
+            this->size = size;
+            this->topIdx = -1;
+            this->ptr = new T[size];
+        }
+
+        Stack(const Stack<T> &other) {
+            this->size = other.size;
+            this->topIdx = other.topIdx;
+            this->ptr = new T[this->size];
+            for (int i = 0; i < this->topIdx + 1; i++) {
+                this->ptr[i] = other.ptr[i];
+            }
+        }
+
+        ~Stack() {
+            delete[] this->ptr;
+        }
+
+        void push(T item) {
+            if (topIdx + 1 < size) {
+                topIdx++;
+                ptr[topIdx] = item;
+            } else {
+                throw std::invalid_argument("Out of bound");
+            }
+        }
+
+        T pop() {
+            if (topIdx == -1) {
+                throw std::invalid_argument("Out of bound");
+            }
+            T valToReturn = this->ptr[topIdx];
+            topIdx--;
+            return valToReturn;
+        }
+
+        T peek() {
+            if (topIdx == -1) {
+                throw std::invalid_argument("Out of bound");
+            }
+            return this->ptr[topIdx];
+        }
+
+        bool isEmpty() {
+            return topIdx == -1;
+        }
+
+        bool isFull() {
+            return topIdx == this->size - 1;
+        }
+
+        void print() {
+            for (int i = 0; i < this->topIdx + 1; i++) {
+                std::cout << this->ptr[i] << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        Stack<T>& operator=(const Stack<T>& other) {
+            if (this == &other) {
+                return *this;
+            }
+            delete[] this->ptr;
+            this->size = other.size;
+            this->topIdx = other.topIdx;
+            this->ptr = new T[this->size];
+            for (int i = 0; i < this->topIdx + 1; i++) {
+                this->ptr[i] = other.ptr[i];
+            }
+            return *this;
+        }
+};
+
+#endif
