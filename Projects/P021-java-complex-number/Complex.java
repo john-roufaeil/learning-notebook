a2b79b07559066200f7993e9ffbee1736d649099
john <john.flouis1@gmail.com>
2025-12-09 13:39:52 +0200

    P021 feat: java generic complex class

---
+++ Projects/P021-java-complex-number/Complex.java
@@ -0,0 +1,52 @@
+public class Complex<T extends Number> {
+    T real;
+    T img;
+
+    public Complex(T real, T img) {
+        this.real = real;
+        this.img = img;
+    }
+
+    public T getReal() {
+        return real;
+    }
+
+    public T getImg() {
+        return img;
+    }
+
+    public void setReal(T real) {
+        this.real = real;
+    }
+
+    public void setImg(T img) {
+        this.img = img;
+    }
+
+    public void print() {
+        Boolean isNegImg = getImg().doubleValue() < 0;
+        Boolean isZeroReal = getReal().doubleValue() == 0;
+        Boolean isZeroImg = getImg().doubleValue() == 0;
+        Boolean isOneImg = getImg().doubleValue() == 1;
+        if (isZeroReal && isZeroImg) System.out.println("0");
+        else {
+            System.out.println(
+                (isZeroReal? "" : getReal()) + 
+                (isNegImg? "" : (isZeroImg || isZeroReal ? "" : "+")) +
+                (isZeroImg? "" : (isOneImg ? (isNegImg ?  "-i" : "i") : getImg() + "i"))
+            );
+        }
+    }
+
+    public Complex<Double> add(Complex<? extends Number> other) {
+        double newReal = getReal().doubleValue() + other.getReal().doubleValue();
+        double newImg = getImg().doubleValue() + other.getImg().doubleValue();
+        return new Complex<>(newReal, newImg);
+    }
+    
+    public Complex<Double> sub(Complex<? extends Number> other) {
+        double newReal = getReal().doubleValue() - other.getReal().doubleValue();
+        double newImg = getImg().doubleValue() - other.getImg().doubleValue();
+        return new Complex<>(newReal, newImg);
+    }
+}
---
+++ Projects/P021-java-complex-number/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        Complex<Integer> c1 = new Complex(3, -2);
+        System.out.print("Complex 1 (integer): ");
+        c1.print();
+
+        Complex<Float> c2 = new Complex(1, -1f);
+        System.out.print("Complex 2 (float): ");
+        c2.print();
+        
+        Complex<Double> c3 = new Complex(5, 5);
+        System.out.print("Complex 3 (double): ");
+        c3.print();
+        
+        Complex<Double> c4 = c1.sub(c2);
+        System.out.print("Complex 4 (double): ");
+        c4.print();
+        Complex<Double> c5 = c1.sub(c3);
+        System.out.print("Complex 5 (double): ");
+        c5.print();
+        
+        Complex<Double> c6 = c2.sub(c1);
+        System.out.print("Complex 6 (double): ");
+        c6.print();
+        Complex<Double> c7 = c2.sub(c3);
+        System.out.print("Complex 7 (double): ");
+        c7.print();
+        
+        Complex<Double> c8 = c3.sub(c1);
+        System.out.print("Complex 8 (double): ");
+        c8.print();
+        Complex<Double> c9 = c3.sub(c2);
+        System.out.print("Complex 9 (double): ");
+        c9.print();
+    }
+}
