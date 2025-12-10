34abb162f4e2d95ae14e0dff527034f1118d1349
john <john.flouis1@gmail.com>
2025-12-10 14:08:36 +0200

    P024 feat: exercise 5 anonymous classes

--- Projects/024-world-stream/Exercise5.java
+++ Projects/024-world-stream/Exercise5.java
@@ -1,17 +1,34 @@
-// import java.util.Objects;
-// import java.util.Optional;
-
-// import static java.lang.System.out;
-// import static java.util.Comparator.comparing;
-// import static java.util.stream.Collectors.maxBy;
+import java.util.Comparator;
+// import java.util.function.Function;
 
 public class Exercise5 {
-
     public static void main(String[] args) {
         CountryDao countryDao = InMemoryWorldDao.getInstance();
         CityDao cityDao = InMemoryWorldDao.getInstance();
-       //write your answer here 
+
+        countryDao.findAllCountries().stream()
+            .map(country -> cityDao.findCityById(country.getCapital()))
+            // .map(new Function<Country, City>() {
+            //     @Override
+            //     public City apply(Country country){
+            //         return cityDao.findCityById(country.getCapital());
+            //     }
+            // })
+            .filter(capital -> capital != null)
+            // .filter(new Predicate<City>() {
+            //     @Override
+            //     public boolean test(City city) {
+            //         return city != null;
+            //     }
+            // })
+            .max(Comparator.comparingInt(City::getPopulation))
+            // .max(Comparator.comparingInt(city -> city.getPopulation()))
+            // .max((city1, city2) -> city1.getPopulation() - city2.getPopulation())
+            .ifPresent(city -> 
+                System.out.println(
+                    "Most populated capital city is " + city.getName() + 
+                    " in " + countryDao.findCountryByCode(city.getCountryCode()).getName() +
+                    " with population of " + city.getPopulation()
+                )); 
     }
-
 }
-
