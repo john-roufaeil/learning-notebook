d06c4e37daac097794335bf83278b751ab8331a4
john <john.flouis1@gmail.com>
2025-12-10 17:15:30 +0200

    P020 feat: use generic T instead of wildcard

--- Projects/020-java-shapes/shapes/Test.java
+++ Projects/020-java-shapes/shapes/Test.java
@@ -2,7 +2,7 @@ package shapes;
 import java.util.ArrayList;
 
 public class Test {
-    public static void testDraw(ArrayList<? extends Shape> shapes) {
+    public static <T extends Shape> void testDraw(ArrayList<T> shapes) {
         for (int i = 0; i < shapes.size(); i++) {
             shapes.get(i).draw();
         }
