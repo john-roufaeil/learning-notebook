769fd4323bc70d5b166aa16ba1e45f55dc102cba
john <john.flouis1@gmail.com>
2025-12-12 23:57:54 +0200

    Java P026 feat: library menu delete all and edit

--- Projects/02-Java/026-library-management/librarymgmt/view/LibraryMenu.java
+++ Projects/02-Java/026-library-management/librarymgmt/view/LibraryMenu.java
@@ -22,8 +22,9 @@ public class LibraryMenu {
             System.out.println("5. View Items");
             System.out.println("6. Edit an Item");
             System.out.println("7. Delete an Item");
-            System.out.println("8. Back");
-            int choice = ConsoleHelper.readChoice("Select option: ", 1, 8);
+            System.out.println("8. Delete all Items");
+            System.out.println("9. Back");
+            int choice = ConsoleHelper.readChoice("Select option: ", 1, 9);
 
             switch (choice) {
                 case 1 -> addBook();
@@ -33,7 +34,8 @@ public class LibraryMenu {
                 case 5 -> viewItems();
                 case 6 -> editItem();
                 case 7 -> deleteItem();
-                case 8 -> { return; }
+                case 8 -> deleteAllItems();
+                case 9 -> { return; }
             }
         }
     }
@@ -140,8 +142,90 @@ public class LibraryMenu {
     }
 
     public static void editItem() {
+    ConsoleHelper.clearConsole();
+    System.out.println("=== Edit a Library Item ===\n");
 
+    int itemId = ConsoleHelper.readInt("Enter Item ID to edit: ");
+    LibraryItem item;
+
+    try {
+        item = itemService.read(itemId);
+    } catch (ObjectNotFoundException e) {
+        System.out.println("\nError: " + e.getMessage());
+        ConsoleHelper.pressEnterToReturn();
+        return;
     }
+
+    boolean done = false;
+    while (!done) {
+        ConsoleHelper.clearConsole();
+        System.out.println("Editing Item ID #" + item.getId());
+        System.out.println("\n+-----------------");
+        System.out.println(item.getItemDetails());
+        System.out.println("\nSelect field to edit:");
+
+        if (item instanceof Book book) {
+            System.out.println("1. Title");
+            System.out.println("2. Author");
+            System.out.println("3. Pages");
+            System.out.println("4. Stock");
+            System.out.println("5. Done");
+
+            int choice = ConsoleHelper.readChoice("Select option: ", 1, 5);
+            try {
+                switch (choice) {
+                    case 1 -> book.setTitle(ConsoleHelper.readString("Enter new title: "));
+                    case 2 -> book.setAuthor(ConsoleHelper.readString("Enter new author: "));
+                    case 3 -> book.setPages(ConsoleHelper.readInt("Enter new page count: "));
+                    case 4 -> book.setStock(ConsoleHelper.readInt("Enter new stock: "));
+                    case 5 -> done = true;
+                }
+            } catch (ObjectNotValidException ex) {
+                System.out.println("\nError: " + ex.getMessage());
+                ConsoleHelper.pressEnterToReturn();
+            }
+
+        } else if (item instanceof Magazine magazine) {
+            System.out.println("1. Title");
+            System.out.println("2. Pages");
+            System.out.println("3. Stock");
+            System.out.println("4. Done");
+
+            int choice = ConsoleHelper.readChoice("Select option: ", 1, 4);
+            try {
+                switch (choice) {
+                    case 1 -> magazine.setTitle(ConsoleHelper.readString("Enter new title: "));
+                    case 2 -> magazine.setPages(ConsoleHelper.readInt("Enter new page count: "));
+                    case 3 -> magazine.setStock(ConsoleHelper.readInt("Enter new stock: "));
+                    case 4 -> done = true;
+                }
+            } catch (ObjectNotValidException ex) {
+                System.out.println("\nError: " + ex.getMessage());
+                ConsoleHelper.pressEnterToReturn();
+            }
+
+        } else if (item instanceof Newspaper newspaper) {
+            System.out.println("1. Title");
+            System.out.println("2. Journal");
+            System.out.println("3. Stock");
+            System.out.println("4. Done");
+
+            int choice = ConsoleHelper.readChoice("Select option: ", 1, 4);
+            try {
+                switch (choice) {
+                    case 1 -> newspaper.setTitle(ConsoleHelper.readString("Enter new title: "));
+                    case 2 -> newspaper.setJournal(ConsoleHelper.readString("Enter new journal: "));
+                    case 3 -> newspaper.setStock(ConsoleHelper.readInt("Enter new stock: "));
+                    case 4 -> done = true;
+                }
+            } catch (ObjectNotValidException ex) {
+                System.out.println("\nError: " + ex.getMessage());
+                ConsoleHelper.pressEnterToReturn();
+            }
+        }
+    }
+}
+
 
     public static void deleteItem() {
         ConsoleHelper.clearConsole();
@@ -156,6 +240,20 @@ public class LibraryMenu {
         }
 
         ConsoleHelper.pressEnterToReturn();
+    }
+
+    public static void deleteAllItems() {
+        ConsoleHelper.clearConsole();
+        System.out.println("=== Delete All Items ===");
+        boolean confirm = ConsoleHelper.confirmAction("This action will delete all items in the library. Are you sure you want to proceed?");
+        if (confirm) {
+            itemService.deleteAll();
+            System.out.println("All library items have been deleted successfully");
+            ConsoleHelper.pressEnterToReturn();
+        } else {
+            System.out.println("Deletion canceled");
+            ConsoleHelper.pressEnterToReturn();
+        }
     }
 }
  
