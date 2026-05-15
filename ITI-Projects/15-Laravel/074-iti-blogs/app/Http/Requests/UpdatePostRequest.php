2079d9f9e774d8be66436419d656c473d4478253
john-roufaeil <john.f.roufaeil@gmail.com>
2026-05-15 13:55:42 +0300

    feat: laravel lab: add validation error messages to blades

--- ITI-Projects/15-Laravel/074-iti-blogs/app/Http/Requests/UpdatePostRequest.php
+++ ITI-Projects/15-Laravel/074-iti-blogs/app/Http/Requests/UpdatePostRequest.php
@@ -11,7 +11,7 @@ class UpdatePostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class UpdatePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title'=> ['required', 'string', 'unique:posts,title'.$this->post->id, 'min:3'],
+            'title'=> ['required', 'string', 'unique:posts,title,'.$this->post, 'min:3'],
             'content'=> ['required', 'string', 'min:10'],
             'author_id'=> ['required', 'string', 'exists:users,id']
         ];
--- ITI-Projects/15-Laravel/074-iti-blogs/resources/views/posts/show.blade.php
+++ ITI-Projects/15-Laravel/074-iti-blogs/resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
     <p><span class="font-bold">Title:</span> {{ $post->title }}</p>
     <p><span class="font-bold">Content:</span> {{ $post->content }}</p>
     <p><span class="font-bold">Author:</span> {{ $post->author->name }}</p>
-    <p><span class="font-bold">Created At:</span> {{ $post->created_at->format('H:i, D d/m/Y') }}</p>
+    <p><span class="font-bold">Created At:</span> {{ $post->created_at->format('l jS \o\f F Y h:i:s A') }}</p>
     <x-button href="{{ route('posts.index') }}" variant="ghost" class="w-fit">Homepage</x-button>
   </div>
 @endsection
